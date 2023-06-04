4a9a40365cf988f03bc36a4a7513b3ce57c44a7a
Kevin Huang <kevwhuang@gmail.com>
2023-06-04 15:55:00 -0500

    0.2 - Implemented data types

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,10 +8,49 @@ interface Config {
 
 interface State {
     authenticated: boolean,
-    flag: string,
     navbar: boolean,
-    notifications: number,
-    page: 'about' | 'contact' | 'guide' | 'home' | 'map' | 'pins' | 'profile' | 'rankings',
+    page: Page,
     power: boolean,
-    theme: 'dark' | 'light',
+    profile: UserProfile,
+    records: UserRecords,
+    settings: UserSettings,
+    tracks: unknown,
 }
+
+interface UserProfile {
+    bio: string,
+    country: string,
+    email: string,
+    first: string,
+    last: string,
+    page: string,
+    photo: string,
+    registered: Date,
+    username: string,
+}
+
+interface UserRecords {
+    experience: bigint,
+    notifications: Notifications[],
+    pins: Pins[],
+}
+
+interface UserSettings {
+    map: number,
+    sync: Date,
+    theme: number,
+    units: number,
+}
+
+type Notifications = {
+    message: string,
+    priority: number,
+    subject: string,
+};
+
+type Page = 'about' | 'contact' | 'guide' | 'home' | 'map' | 'pins' | 'profile' | 'rankings';
+
+type Pins = {
+    lat: number,
+    long: number,
+};
--- src/hooks/useZustand.ts
+++ src/hooks/useZustand.ts
@@ -1,13 +1,35 @@
 import { create } from 'zustand';
 
+import seed from '../data/seed.json';
+
 const initialize: State = {
-    authenticated: false,
-    flag: 'US',
+    authenticated: true,
     navbar: false,
-    notifications: 0,
-    page: 'pins',
+    page: 'home',
     power: false,
-    theme: 'dark',
+    profile: {
+        bio: seed[0].profile.bio || '',
+        country: seed[0].profile.country || 'US',
+        email: seed[0].profile.email || '',
+        first: seed[0].profile.first || '',
+        last: seed[0].profile.last || '',
+        page: seed[0].profile.page || '',
+        photo: seed[0].profile.photo || '',
+        registered: seed[0].profile.registered || new Date,
+        username: seed[0].profile.username || '',
+    },
+    records: {
+        experience: BigInt(seed[0].records.experience) || 0n,
+        notifications: seed[0].records.notifications || [],
+        pins: seed[0].records.pins || {},
+    },
+    settings: {
+        map: seed[0].settings.map || 2,
+        sync: seed[0].settings.sync || new Date,
+        theme: seed[0].settings.theme || 1,
+        units: seed[0].settings.units || 1,
+    },
+    tracks: null,
 };
 
 const useZustand = create<Actions & State>(set => ({
