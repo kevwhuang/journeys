180e24702c9d4c8d261e6cdb4830ff70c4815372
Kevin Huang <kevwhuang@gmail.com>
2023-06-09 15:16:30 -0500

    Intersection observer api to trigger animations

--- src/components/elements/ActionsHeadings.tsx
+++ src/components/elements/ActionsHeadings.tsx
@@ -10,23 +10,35 @@ function ActionsHeadings(): React.ReactElement {
     const { inView, ref } = useInView(options);
 
     return (
-        <section
-            className={inView ? 'actions__headings play' : 'actions__headings'}
-            ref={ref}
-        >
+        <section className="actions__headings" ref={ref}>
             <div className="actions__headings--container">
-                <span>WALK</span>
-                <span>BIKE</span>
-                <span>RIDE</span>
-                <span>DRIVE</span>
-                <span>SAIL</span>
-                <span>FLY</span>
-                <span>the <u>choice</u> is <u>yours…</u></span>
-                <span>see<u>k</u>&nbsp;adventure
+                <span className={inView ? 'play' : ''}>WALK</span>
+                <span className={inView ? 'play' : ''}>BIKE</span>
+                <span className={inView ? 'play' : ''}>RIDE</span>
+                <span className={inView ? 'play' : ''}>DRIVE</span>
+                <span className={inView ? 'play' : ''}>SAIL</span>
+                <span className={inView ? 'play' : ''}>FLY</span>
+                <span className={inView ? 'play' : ''}>
+                    the&nbsp;
+                    <u>choice</u>
+                    &nbsp;is&nbsp;
+                    <u>
+                        <i className={inView ? 'play' : ''}>y</i>
+                        <i className={inView ? 'play' : ''}>o</i>
+                        <i className={inView ? 'play' : ''}>u</i>
+                        <i className={inView ? 'play' : ''}>r</i>
+                        <i className={inView ? 'play' : ''}>s&nbsp;</i>
+                        …
+                    </u>
+                </span>
+                <span className={inView ? 'play' : ''}>
+                    see
+                    <u className={inView ? 'play' : ''}>k&nbsp;</u>
+                    adventure
                     <img
+                        className={inView ? 'play' : ''}
                         src={inView ? '/assets/rotating-earth.gif' : ''}
                         draggable="false"
-                        loading="lazy"
                     />
                 </span>
             </div>
