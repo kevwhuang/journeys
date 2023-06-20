099e60808ed97a6288b0776fbc34f0ae3afaa490
Kevin Huang <kevwhuang@gmail.com>
2023-06-20 16:46:56 -0500

    0.8 - GMP reverse geocoding

--- src/components/elements/PointSummary.tsx
+++ src/components/elements/PointSummary.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import useZustand from '../../hooks/useZustand';
 
@@ -8,11 +9,26 @@ interface Props {
 
 function PointSummary(props: Props): React.ReactElement {
     const pins = useZustand(s => s.pins);
+    const [address, setAddress] = React.useState('');
     const { lat, long } = pins?.[props.id - 1];
+
+    React.useEffect(() => {
+        (async function get() {
+            const res = await axios('/.netlify/functions/gmpReverseGeocoding',
+                {
+                    headers: {
+                        'x-coordinates': `latlng=${lat},${long}`,
+                    },
+                })
+                .catch(err => console.log(err));
+
+            res && setAddress(res.data);
+        }());
+    }, [lat, long]);
 
     return (
         <section className="point__summary">
-            <p>{'{address}'}</p>
+            <p>{address}</p>
             {lat
                 && <p>
                     lat: {lat}
